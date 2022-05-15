7ec0d0cddbb1dd197a80926595240675c06040c1
Admin <Admin@ASUS-TUF-DASH-F>
2022-05-15 14:25:13 +0200

    Add project files.

---
+++ kalkulator funkcje/kalkulator funkcje.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int table_size = 20;
+	int table[table_size];
+	char i;
+	int zmienna;
+	char op;
+
+	for (i = 0; i < table_size; i++)
+	{
+		cout << "Podaj " << i + 1 << " liczbe, aby przerwac podaj 0  " << endl;
+
+		cin >> zmienna;
+
+		if (zmienna == 0)
+		{
+			break;
+		}
+		else
+		{
+			table[i] = zmienna;
+		}
+	}
+
+	cout << "podaj operacje" << endl;
+	cin >> op;
+
+	return table, table_size, op, zmienna, i;
+}
+
+string dodawanie(int table, char i, int zmienna, char op, const int table_size)
+{
+
+	for (i = 0; i < table_size; i++)
+	{
+		switch (op)
+		{
+		case '+':
+		{
+			int wynik = 0;
+			for (int j = 0; j < i; j++)
+			{
+				wynik += table[j];
+			}
+			cout << "wynik operacji " << op << ": " << wynik << endl;
+			break;
+		}
+		}
+	}
+}
